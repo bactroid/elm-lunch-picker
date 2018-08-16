11e9c773239ad6ab86709f13eff3e72e0a8eb692
Kate Haskell <fuzzcat@bactroid.net>
2018-08-16 18:48:12 -0400

    Fixed build script to remove references to dist

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp src/index.html dist/index.html
-cp src/app.css dist/app.css
-elm-make src/App.elm --output=dist/app.js
+cp src/index.html docs/
+cp src/app.css docs/
+elm-make src/App.elm --output=docs/app.js
 
 echo "Project build complete"
